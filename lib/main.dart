0f9fdd56005766efefd0bca91b8ded7024a1ed43
MinaYasser100 <minayasser43@gmail.com>
2025-09-11 21:06:26 +0300

    add firebase to the app

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,12 @@ import 'package:david_psalmist/core/routing/app_router.dart';
 import 'package:david_psalmist/core/utils/theme_data_func.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   setupDependencies();
   runApp(const MyApp());
 }
