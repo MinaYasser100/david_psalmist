f71bc8bbf2d9211e98601d785648be86f3270fb7
MinaYasser100 <minayasser43@gmail.com>
2025-12-25 00:56:13 +0200

    some edit

--- lib/features/attendance/data/repo/attendance_repo.dart
+++ lib/features/attendance/data/repo/attendance_repo.dart
@@ -1,13 +1,20 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dartz/dartz.dart';
 import 'package:david_psalmist/core/firebase/firebase_firestore_error_handler.dart';
 import 'package:david_psalmist/core/model/attendance_model/attendance_model.dart';
 import 'package:david_psalmist/core/model/student_model/student_model.dart';
 import 'package:david_psalmist/features/attendance/data/service/attendance_services.dart';
+import 'package:easy_localization/easy_localization.dart';
 
 abstract class AttendanceRepo {
   Stream<Either<String, List<AttendanceModel>>> getAttendanceStream(
     StudentModel studentModel,
   );
+
+  Future<Either<String, String>> deleteAttendanceRecord({
+    required StudentModel studentModel,
+    required AttendanceModel attendanceModel,
+  });
 }
 
 class AttendanceRepoImpl implements AttendanceRepo {
@@ -41,8 +48,30 @@ class AttendanceRepoImpl implements AttendanceRepo {
 
         return Right(attendanceRecords);
       });
+    } on FirebaseException catch (e) {
+      return Stream.value(
+        Left(firestoreErrorHandler.mapFirebaseFirestoreException(e)),
+      );
     } catch (e) {
       return Stream.value(Left(e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<String, String>> deleteAttendanceRecord({
+    required StudentModel studentModel,
+    required AttendanceModel attendanceModel,
+  }) async {
+    try {
+      await attendanceServices.deleteAttendanceRecord(
+        studentModel: studentModel,
+        attendanceModel: attendanceModel,
+      );
+      return Right('Attendance record deleted successfully'.tr());
+    } on FirebaseException catch (e) {
+      return Left(firestoreErrorHandler.mapFirebaseFirestoreException(e));
+    } catch (e) {
+      return Left('Unknown error occurred while deleting attendance'.tr());
     }
   }
 }
--- lib/features/attendance/data/service/attendance_services.dart
+++ lib/features/attendance/data/service/attendance_services.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:david_psalmist/core/model/attendance_model/attendance_model.dart';
 import 'package:david_psalmist/core/model/student_model/student_model.dart';
 import 'package:david_psalmist/core/utils/constant.dart';
 
@@ -17,5 +18,21 @@ class AttendanceServices {
         .collection(ConstantVariable.attendanceCollection)
         .orderBy('date', descending: true)
         .snapshots();
+  }
+
+  Future<void> deleteAttendanceRecord({
+    required StudentModel studentModel,
+    required AttendanceModel attendanceModel,
+  }) async {
+    await _firestore
+        .collection(ConstantVariable.levelsCollection)
+        .doc(studentModel.levelId)
+        .collection(ConstantVariable.classesCollection)
+        .doc(studentModel.classId)
+        .collection(ConstantVariable.studentsCollection)
+        .doc(studentModel.studentId)
+        .collection(ConstantVariable.attendanceCollection)
+        .doc(attendanceModel.id)
+        .delete();
   }
 }
